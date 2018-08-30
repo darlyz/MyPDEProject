749b1a2a2f47f060a1956258c076d72831f5a698
lich zh <darlyz@163.com>
2018-08-30 19:39:58 +0800

    add

--- test.c
+++ test.c
@@ -5,20 +5,20 @@ void ReadNodeMesh(char*);
 
 int main()
 {
-	ReadJson("testpro.json");
-	
-	if(NodMsh==NULL) printf("NodMsh is not creat\n");
-	if(EdgMsh==NULL) printf("EdgMsh is not creat\n");
-	if(FacMsh==NULL) printf("FacMsh is not creat\n");
-	if(VolMsh==NULL) printf("VolMsh is not creat\n");
-	
-	FieldNum=2;
-	ReadNodeMesh("test.dat");
-	printf("%d\n",NodMsh[3].Mesh_Topo[(NodMsh[3].Mesh_Scale*NodMsh[3].Elem_CompN)-2]);
-	
-	//if(NodMsh!=NULL) free(NodMsh);
-	//if(EdgMsh!=NULL) free(EdgMsh);
-	//if(FacMsh!=NULL) free(FacMsh);
-	//if(VolMsh!=NULL) free(VolMsh);
-	return 1;
+    ReadJson("testpro.json");
+    
+    if(NodMsh==NULL) printf("NodMsh is not creat\n");
+    if(EdgMsh==NULL) printf("EdgMsh is not creat\n");
+    if(FacMsh==NULL) printf("FacMsh is not creat\n");
+    if(VolMsh==NULL) printf("VolMsh is not creat\n");
+    
+    FieldNum=2;
+    ReadNodeMesh("test.dat");
+    printf("%d\n",NodMsh[3].Mesh_Topo[(NodMsh[3].Mesh_Scale*NodMsh[3].Elem_CompN)-2]);
+    
+    //if(NodMsh!=NULL) free(NodMsh);
+    //if(EdgMsh!=NULL) free(EdgMsh);
+    //if(FacMsh!=NULL) free(FacMsh);
+    //if(VolMsh!=NULL) free(VolMsh);
+    return 1;
 }
--- testcjson.c
+++ testcjson.c
@@ -6,27 +6,27 @@
 
 int main(int argc, char* argv[])
 {
-	FILE *fp;
-	if((fp=fopen(argv[1],"r"))==NULL)
-		return -1;
-	
-	char str[255];
-	int i=0;
-	while(fgets(str,255,fp) != NULL)
-	{
-		i+=sizeof(str);  //printf("%s\n",str);
-		
-	}//printf("%d\n",i);
-	
-	rewind(fp);
+    FILE *fp;
+    if((fp=fopen(argv[1],"r"))==NULL)
+        return -1;
+    
+    char str[255];
+    int i=0;
+    while(fgets(str,255,fp) != NULL)
+    {
+        i+=sizeof(str);  //printf("%s\n",str);
+        
+    }//printf("%d\n",i);
+    
+    rewind(fp);
 
-	char *data;
-	data = (char*)calloc(sizeof(char),i+1);
-	while(fgets(str,255,fp) != NULL)
-		strcat(data,str);
-	fclose(fp);
-	//printf("%d\t%s\n",i,data);
-	
+    char *data;
+    data = (char*)calloc(sizeof(char),i+1);
+    while(fgets(str,255,fp) != NULL)
+        strcat(data,str);
+    fclose(fp);
+    //printf("%d    %s\n",i,data);
+    
     //char *data = "{\"love\":[\"LOL\",\"Go shopping\"]}";
     //从缓冲区中解析出JSON结构
     cJSON * json= cJSON_Parse(data);
@@ -34,16 +34,16 @@ int main(int argc, char* argv[])
     //将传入的JSON结构转化为字符串 并打印
     char *json_data = NULL;
     printf("data:%s\n",json_data = cJSON_Print(json));
-	
-	cJSON *RegionMesh = cJSON_GetObjectItem(json,"RegionMesh");
-	cJSON *BndaryMesh = cJSON_GetObjectItem(json,"BndaryMesh");
-	char  *RegMesh    = cJSON_Print(RegionMesh);
-	char  *BndMesh    = cJSON_Print(BndaryMesh);
-	printf("RegionMesh:%s\n",RegMesh);
-	printf("BndaryMesh:%s\n",BndMesh);
-	
-	char *pattern="[A-Z][0-9]+";
-	
+    
+    cJSON *RegionMesh = cJSON_GetObjectItem(json,"RegionMesh");
+    cJSON *BndaryMesh = cJSON_GetObjectItem(json,"BndaryMesh");
+    char  *RegMesh    = cJSON_Print(RegionMesh);
+    char  *BndMesh    = cJSON_Print(BndaryMesh);
+    printf("RegionMesh:%s\n",RegMesh);
+    printf("BndaryMesh:%s\n",BndMesh);
+    
+    char *pattern="[A-Z][0-9]+";
+    
     
     free(json_data);
     //将JSON结构所占用的数据空间释放
